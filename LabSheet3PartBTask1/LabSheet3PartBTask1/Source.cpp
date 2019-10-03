2523e72cf044fd4579a802e44f935343a8c6ba39
X00139120 Andrew Teeters <x00139120@COMPUTING.STU.IT-TALLAGHT.IE>
2019-10-03 13:35:10 +0100

    labsheet3partb task 2 started, task 1 finished

--- LabSheet3PartBTask1/LabSheet3PartBTask1/Source.cpp
+++ LabSheet3PartBTask1/LabSheet3PartBTask1/Source.cpp
@@ -1,21 +1,22 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 
-bool fillArray();
+bool fillArray(char characters[], int& counter);
 
-void outputInReverse() {
-
-}
+void outputInReverse(char characters[], int& counter);
 
 int main() {
 
 
 
+	char characters[100];
+	int counter = 0;
 
 
-
-	
+	fillArray(characters, counter);
+	outputInReverse(characters, counter);
 
 
 
@@ -26,16 +27,14 @@ int main() {
 	return 0;
 }
 
-bool fillArray() {
-	char characters[100];
+bool fillArray(char characters[], int& counter) {
 	char control = 'x';
-	int counter = 0;
 
 	cout << "Please input characters, input a '.' to end the input" << endl;
 	while (control != '.') {
 		cin >> control;
 		if (control != '.') {
-			cin >> characters[counter];
+			characters[counter] = control;
 			counter++;
 		} 
 	}
@@ -47,4 +46,19 @@ bool fillArray() {
 	else {
 		return false;
 	}
+}
+
+
+void outputInReverse(char characters[], int& counter) {
+	
+	char charreverse[100];
+	int size = counter;
+	for (int i = 0; i < size; i++)
+	{
+		charreverse[i] = characters[counter - 1];
+		cout << charreverse[i] << endl;
+		counter--;
+	}
+	
+	
 }
